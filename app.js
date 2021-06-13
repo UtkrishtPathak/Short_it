49cc8cd29e8ac3eebd43a8d8dab9620df3532643
UtkrishtPathak <utkrishtpathak@gmail.com>
2021-06-13 22:33:42 +0530

    Home page done

--- app.js
+++ app.js
@@ -8,8 +8,10 @@ app.set("view engine","ejs");
 
 app.get("/", (req,res) => {
     res.render("home");
-})
+});
+
+app.post
 
 app.listen(1000, function(){
     console.log("Running on port 1000");
-})
+});
