e593188c6cefe4ccb393c7766acbff0bb0383831
UtkrishtPathak <utkrishtpathak@gmail.com>
2021-06-13 15:15:04 +0530

    setting up home page

---
+++ app.js
@@ -0,0 +1,15 @@
+
+const express   = require("express"),
+      app       = express(),
+      mongoose  = require("mongoose");
+
+
+app.set("view engine","ejs");
+
+app.get("/", (req,res) => {
+    res.render("home");
+})
+
+app.listen(1000, function(){
+    console.log("Running on port 1000");
+})
